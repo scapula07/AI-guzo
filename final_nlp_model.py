f4a48d9b5cbcb9e4547ff8ff574079d208c9e2c3
Rishi-Prasadha <r.prasadha_17@yahoo.com>
2024-01-11 11:59:56 -0600

    first commit

---
+++ final_nlp_model.py
@@ -0,0 +1,21 @@
+#   imports
+import numpy as np
+import pandas as pd
+from os import path
+import sys
+from flask import Flask
+
+#   import NLP
+import yake
+yake_extractor = yake.KeywordExtractor()
+
+def keywords(input_text):
+    if len(input_text) > 240:
+        result = "Input too long, please try again"
+    else:
+        keywords = yake_extractor.extract_keywords(text_input)
+
+        keywords_df = pd.DataFrame(keywords, columns=['term', 'score'])
+        keywords_df = keywords_df.sort_values(by='score') # The lower the score the more relevant the keyword is
+        result = keywords_df[0][0:5]
+    return result
